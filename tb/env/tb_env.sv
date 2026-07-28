c752ba526c52505b2057ad67ed48b330c638d2e5
pratikshaa-07 <pratikshashetty125@gmail.com>
2026-07-28 12:49:40 +0530

    Create tb_env.sv

---
+++ tb/env/tb_env.sv
@@ -0,0 +1,26 @@
+class environment extends uvm_env;
+  active_agent a_agt;
+  passive_agent p_agt;
+  scoreboard scb;
+  subscriber sub;
+
+  `uvm_component_utils(environment)
+  
+  function new(string name="environment",uvm_component parent);
+    super.new(name,parent);
+  endfunction
+  
+  virtual function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    a_agt=passive_agent::type_id::create("a_agt",this);
+    p_agt=active_agent::type_id::create("p_agt",this);
+    scb=scoreboard::type_id::create("scb",this);
+    sub=subscriber::type_id::create("sub",this); 
+  endfunction
+  
+  virtual function void connect_phase(uvm_phase phase);    
+    agt.monitor.mon_port.connect(scb.sco_port.analysis_export);
+    agt.monitor.mon_port.connect(sub.cov_port.analysis_export);
+    endfunction
+endclass
+      
