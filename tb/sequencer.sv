69f74fca1d43fce8abf84d48753fd053e95c9425
pratikshaa-07 <pratikshashetty125@gmail.com>
2026-07-28 12:30:48 +0530

    Add sequencer class for UVM sequence

---
+++ tb/sequencer.sv
@@ -0,0 +1,8 @@
+class sequencer extends uvm_sequence#(seq_item);
+  `uvm_component_utils(sequencer)
+  
+  function new(string name="",uvm_component parent);
+    super.new(name="",parent);
+  endfunction
+  
+endclass
