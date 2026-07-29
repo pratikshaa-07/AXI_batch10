6a72dd4868d1ffd3cd29e7aed505a3f0878251d8
pratikshaa-07 <pratikshashetty125@gmail.com>
2026-07-29 12:40:37 +0530

    Rename analysis port from pas_port to send_port

--- tb/tb_passive_agent/passive_monitor.sv
+++ tb/tb_passive_agent/passive_monitor.sv
@@ -3,13 +3,13 @@ class passive_monitor extends uvm_monitor;
   `uvm_component_utils(passive_monitor)
  
   virtual inf.MON vif;
-  uvm_analysis_port #(seq_item) pas_port;
+  uvm_analysis_port #(seq_item) send_port;
  
   seq_item tr;
  
   function new(string name = "", uvm_component parent);
     super.new(name, parent);
-    send_port = new("pas_port", this);
+    send_port = new("send_port", this);
   endfunction
  
   function void build_phase(uvm_phase phase);
@@ -24,7 +24,7 @@ class passive_monitor extends uvm_monitor;
       tr = seq_item::type_id::create("tr");
       tr.rd_an   = vif.mon_cb.rd_en;
       tr.rd_data = vif.mon_cb.rd_data;
-      pass_port.write(tr);
+      send_port.write(tr);
       @(vif.mon_cb);
     end
   endtask
