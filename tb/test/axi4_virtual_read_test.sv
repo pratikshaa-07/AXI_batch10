71ed41a720f51026e972a68bce7f2b1c511f0094
pratikshaa-07 <pratikshashetty125@gmail.com>
2026-07-29 14:15:30 +0530

    Update axi4_virtual_read_test.sv

--- tb/test/axi4_virtual_read_test.sv
+++ tb/test/axi4_virtual_read_test.sv
@@ -22,7 +22,7 @@ class axi4_virtual_read_test extends base_test;
     seq = axi4_virtual_read_seq::type_id::create("seq");
     if (!seq.randomize() with { num_txns == 10; })
       `uvm_error(get_type_name(), "randomize failed")
-    seq.start(env_h.vseqr_h);
+    seq.start(env.vseqr);
 
     phase.drop_objection(this);
   endtask
