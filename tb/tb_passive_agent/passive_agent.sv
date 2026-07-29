2f8b2b3a03a6828f68b92ecc7ebb764b1993ef25
pratikshaa-07 <pratikshashetty125@gmail.com>
2026-07-29 12:43:02 +0530

    Update passive_agent.sv

--- tb/tb_passive_agent/passive_agent.sv
+++ tb/tb_passive_agent/passive_agent.sv
@@ -1,1 +1,15 @@
+class passive_agent extends uvm_agent;
+  passive_monitor mon;
 
+  `uvm_component_utils(passive_agent)
+
+  function new(string name="",uvm_component parent);
+    super.new(name,parent);
+  endfunction
+
+  virtual function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    mon=passive_monitor::type_id::create("mon",this);
+  endfunction
+  
+endclass
