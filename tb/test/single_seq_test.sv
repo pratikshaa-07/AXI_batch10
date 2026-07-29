126a4db89f9c0959109e0a9690983d3a86767cab
pratikshaa-07 <pratikshashetty125@gmail.com>
2026-07-29 12:06:26 +0530

    Create single_seq_test.sv

---
+++ tb/test/single_seq_test.sv
@@ -0,0 +1,26 @@
+//--------------------------------------------------------------------------------------------
+// Class: single_seq_test
+// Minimal test for running exactly ONE cpu_write_seq item - bypasses top_vseq
+// entirely, starts directly on the cpu sequencer.
+//--------------------------------------------------------------------------------------------
+class single_seq_test extends base_test;
+
+  `uvm_component_utils(single_seq_test)
+
+  function new(string name = "", uvm_component parent);
+    super.new(name, parent);
+  endfunction
+
+  task run_phase(uvm_phase phase);
+    cpu_write_seq seq;
+    phase.raise_objection(this);
+
+    seq = cpu_write_seq::type_id::create("seq");
+    
+    //change it
+    seq.start(env_h.cpu_env_h.cpu_agt_h.cpu_seqr_h);
+
+    phase.drop_objection(this);
+  endtask
+
+endclass
