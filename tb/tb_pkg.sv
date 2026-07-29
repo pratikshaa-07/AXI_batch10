339a6c3e51d39f05cd67e4a254b526ab3bf71203
pratikshaa-07 <pratikshashetty125@gmail.com>
2026-07-29 14:40:19 +0530

    Update tb_pkg.sv

--- tb/tb_pkg.sv
+++ tb/tb_pkg.sv
@@ -23,8 +23,8 @@ package cpu_tb_pkg;
   `include "tb_active_agent/driver.sv"
   `include "tb_active_agent/active_monitor.sv"
   `include "cpu_agent/active_agent.sv"
-  `include "cpu_agent/cpu_write_seq.sv"
-  `include "cpu_agent/cpu_read_seq.sv"   // ADDED: cpu-side read sequence
+  `include "cpu_agent/write_seq.sv"
+  `include "cpu_agent/read_seq.sv"   // ADDED: cpu-side read sequence
 
   `include "tb_passive_agent/passive_agent.sv"
   `include "tb_passive_agent/passive_monitor.sv"
