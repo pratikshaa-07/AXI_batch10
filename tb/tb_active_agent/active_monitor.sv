7d1ce6dbfc1a81dd6e6d0721761f2427f15bfcd8
pratikshaa-07 <pratikshashetty125@gmail.com>
2026-08-19 15:22:29 +0530

    Update active_monitor.sv

--- tb/tb_active_agent/active_monitor.sv
+++ tb/tb_active_agent/active_monitor.sv
@@ -18,7 +18,7 @@ class active_monitor extends uvm_monitor;
   endfunction
  
   task run_phase(uvm_phase phase);
-    repeat(4) @(vif.mon_cb);
+    repeat(3) @(vif.mon_cb);
     forever begin
       tr = seq_item::type_id::create("tr");
       tr.wr_en   = vif.mon_cb.wr_en;
